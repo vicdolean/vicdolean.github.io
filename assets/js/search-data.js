89f2e059eee20c54f3bc70250ca20947bd0b32f4
vicdolean <64419844+vicdolean@users.noreply.github.com>
2026-01-25 13:20:45 +0000

    Deploying to gh-pages from @ vicdolean/vicdolean.github.io@878c35f221fe2184f998e1a60fa7f945ef1bf3de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,13 +479,6 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=ZzHAiHkAAAAJ", "_blank");
         },
       },{
-        id: 'social-arxiv',
-        title: 'arXiv',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://arxiv.org/a/dolean_v_1.html", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
